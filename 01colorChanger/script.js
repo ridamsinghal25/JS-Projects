44f88cab07cf941ceae42f654262c3669abfa3bb
Ridam-Singhal <manusinghal84@gmail.com>
2023-12-22 15:37:43 +0530

    new approach for colorchanger

--- 01colorChanger/script.js
+++ 01colorChanger/script.js
@@ -1,20 +1,45 @@
-const grey = document.getElementById("grey");
-const white = document.getElementById("white");
-const blue = document.getElementById("blue");
-const yellow = document.getElementById("yellow");
+// const grey = document.getElementById("grey");
+// const white = document.getElementById("white");
+// const blue = document.getElementById("blue");
+// const yellow = document.getElementById("yellow");
 
-grey.addEventListener("click", () => {
-  document.body.style.backgroundColor = "grey";
-});
+// grey.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "grey";
+// });
 
-white.addEventListener("click", () => {
-  document.body.style.backgroundColor = "white";
-});
+// white.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "white";
+// });
 
-blue.addEventListener("click", () => {
-  document.body.style.backgroundColor = "blue";
-});
+// blue.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "blue";
+// });
 
-yellow.addEventListener("click", () => {
-  document.body.style.backgroundColor = "yellow";
+// yellow.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "yellow";
+// });
+
+// Approach 2
+
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
 });
