86d7d81548604400b20428bb676baba5a6344685
Ridam-Singhal <manusinghal84@gmail.com>
2024-03-14 16:12:20 +0530

    added project

--- 09cats/script.js
+++ 09cats/script.js
@@ -1,24 +1,66 @@
 const url = "https://api.thecatapi.com/v1/images/search";
 const container = document.querySelector(".container");
-const btn = document.querySelector(".btn");
+let width;
+const windowWidth = window.innerWidth;
+if (windowWidth <= 510) {
+  width = window.innerWidth - 20;
+  container.style.width = `${width}px`;
+} else {
+  width = 500;
+}
 
-btn.addEventListener("click", () => {
-  fetch(url)
-    .then((data) => {
-      return data.json();
-    })
-    .then((res) => {
-      if (container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
+document.querySelector(".btn").addEventListener("click", getImg);
+const catsContainer = document.getElementsByClassName("cats")[0];
 
-      const catUrl = res[0].url;
+async function getImg() {
+  let response = await fetch(url);
+  let data = await response.json();
+  showImg(data[0].url);
+}
+let imgCount = 0;
+function showImg(imgUrl) {
+  const imgElement = document.createElement("img");
+  imgElement.src = imgUrl;
+  imgElement.alt = "Random Cat";
+  imgElement.classList.add("random_cats");
+  imgElement.style.width = `${width}`;
 
-      const imgElement = document.createElement("img");
-      imgElement.setAttribute("src", catUrl);
-      imgElement.classList.add("cats");
-      imgElement.classList.add("random_cats");
+  catsContainer.appendChild(imgElement);
+  imgCount = catsContainer.children.length - 1;
+  catsContainer.style.transform = `translateX(-${width * imgCount}px)`;
 
-      container.appendChild(imgElement);
+  if (imgCount === 1) {
+    createNavBtn();
+    const navBtns = document.querySelectorAll(".nav-btn");
+    navBtns.forEach((btn) => {
+      btn.addEventListener("click", navigation, false);
     });
-});
+  }
+}
+
+function navigation(e) {
+  let target = e.currentTarget;
+  if (imgCount > 0 && target.classList.contains("prev")) {
+    imgCount--;
+    catsContainer.style.transform = `translateX(-${width * imgCount}px)`;
+  } else if (
+    imgCount < catsContainer.children.length - 1 &&
+    target.classList.contains("next")
+  ) {
+    imgCount++;
+    catsContainer.style.transform = `translateX(-${width * imgCount}px)`;
+  }
+}
+
+function createNavBtn() {
+  let prevBtn = document.createElement("div");
+  let nextBtn = document.createElement("div");
+  prevBtn.classList.add("prev", "nav-btn", "btn");
+  nextBtn.classList.add("next", "nav-btn", "btn");
+  prevBtn.innerHTML = `<i class="fa-solid fa-arrow-left"></i>`;
+  nextBtn.innerHTML = `<i class="fa-solid fa-arrow-right"></i>`;
+
+  container.appendChild(nextBtn);
+  container.appendChild(prevBtn);
+}
+// setInterval(getImg, 2000);
---
+++ 10crudDom/script.js
@@ -0,0 +1,60 @@
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const year = document.getElementById("year");
+const bookList = document.getElementById("book-list");
+const btn = document.querySelector(".btn");
+
+// Load existing data from local storage
+bookList.innerHTML = localStorage.getItem("data");
+attachDeleteListeners(); // Attach event listeners after loading existing data
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (title.value == "" || author.value == "" || year.value == "") {
+    alert("Please fil the input correctly");
+    return;
+  }
+
+  const newRow = document.createElement("section");
+  const titleCell = document.createElement("div");
+  const authorCell = document.createElement("div");
+  const yearCell = document.createElement("div");
+  const dltBtn = document.createElement("span");
+  dltBtn.classList.add("dlt-btn");
+
+  titleCell.textContent = title.value;
+  authorCell.textContent = author.value;
+  yearCell.textContent = year.value;
+  dltBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+
+  newRow.appendChild(titleCell);
+  newRow.appendChild(authorCell);
+  newRow.appendChild(yearCell);
+  newRow.appendChild(dltBtn);
+  bookList.appendChild(newRow);
+  dltBtn.addEventListener("click", dlt);
+
+  title.value = "";
+  author.value = "";
+  year.value = "";
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("data", bookList.innerHTML);
+}
+function dlt(e) {
+  let target = e.currentTarget;
+  target.parentNode.remove();
+  updateLocalStorage();
+}
+
+function attachDeleteListeners() {
+  // Attach event listeners to delete buttons
+  // cause after refreshing the website eventListeners are deleted
+  const deleteButtons = document.querySelectorAll(".dlt-btn");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", dlt);
+  });
+}
